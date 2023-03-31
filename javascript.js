3b7d28e07809e1ede6778a470b5d1f41aa5f4a09
juliochavezpro <juliochavezpro@gmail.com>
2023-03-31 13:49:59 -0400

    Stop game after five rounds

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const gameOptions = ["rock", "paper", "scissors"];
-let playerScore = '';
-let computerScore = '';
+let playerScore = 0;
+let computerScore = 0;
 
 let playerSelection;
 let computerSelection;
@@ -15,7 +15,6 @@ function playRound(e) {
     let userRoundVictory = `You picked ${playerSelection} and the computer picked ${computerSelection}. You win the round!`;
     let errorMessage = "User input is invalid. Please select Rock, Paper or Scissors.";
     let tieMessage = `You picked ${playerSelection} and the computer picked ${computerSelection}. The round is a tie!`;
-    updateScore();
     if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore += 1;
         document.getElementById("results").innerHTML = `${userRoundVictory}`
@@ -39,6 +38,8 @@ function playRound(e) {
     } else {
         document.getElementById("results").innerHTML = `${errorMessage}`
     }
+    document.getElementById("score").innerText = `Player: ${playerScore} Computer: ${computerScore}`
+    endGame();
 };
 
 const buttons = document.querySelectorAll('.choices');
@@ -46,7 +47,17 @@ buttons.forEach((btn) => {
     btn.addEventListener("click", playRound)
 });
 
-
-function updateScore () {
-    document.getElementById("score").innerText = `Player: ${playerScore} Computer: ${computerScore}`
-};
+function endGame() {
+    if (playerScore == 5) {
+        document.getElementById("results").innerHTML = `Congratulations! You won five rounds! Pick again to start over.`;
+        document.getElementById("score").innerText = `Your final score: ${playerScore} Computer Final Score: ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore == 5) {
+        document.getElementById("results").innerHTML = `Oh no! The computer won five rounds! Pick again to start over.`
+        document.getElementById("score").innerText = `Your final score: ${playerScore} Computer Final Score: ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
